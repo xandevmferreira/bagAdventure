56069380dd1cac3ec215ea4ae248acabf6d19ed9
Alexandre Ferreira <alexandrevmferreira@gmail.com>
2023-03-24 14:36:43 -0300

    local storage, usando JSON.stringfy para transformar itens em string,criando constante item

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade =  evento.target.elements['quantidade']
-
+    
     criaElemento(nome.value, quantidade.value)
 
    nome.value = ""
@@ -19,12 +20,18 @@ function criaElemento (nome, quantidade) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = quantidade
-
     novoItem.appendChild(numeroItem)
+    
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
 
-    localStorage.setItem("nome", nome)
-    localStorage.setItem("quantidade", quantidade)
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
