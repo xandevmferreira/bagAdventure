43b92d6facd99b5ae0d90745843e376b4a6524ee
Alexandre Ferreira <alexandrevmferreira@gmail.com>
2023-03-31 20:18:57 -0300

    adicionando mudança e adição na lista de itens

--- js/main.js
+++ js/main.js
@@ -1,37 +1,66 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || [] 
+
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade =  evento.target.elements['quantidade']
-    
-    criaElemento(nome.value, quantidade.value)
 
-   nome.value = ""
-   quantidade.value = ""
-})
-
-function criaElemento (nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add('item')
-
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-    novoItem.appendChild(numeroItem)
-    
-    novoItem.innerHTML += nome
-
-    lista.appendChild(novoItem)
+    const existe = itens.find (elemento => elemento.nome === nome.value)
 
     const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
+
+    if(existe) {
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+        
+        itens [existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+    
+   
+
+    criaElemento(itemAtual)
 
     itens.push(itemAtual)
 
-    localStorage.setItem("item", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento (item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
+
+    novoItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
+
+    
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
